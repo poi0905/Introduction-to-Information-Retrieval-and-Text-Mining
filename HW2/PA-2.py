ca234c0a969261ff93df0428a3f20bb50df5e4d6
poi0905 <asdfg3021524@gmail.com>
2020-10-08 00:12:47 +0800

    2020/10/8

--- HW2/PA-2.py
+++ HW2/PA-2.py
@@ -7,4 +7,25 @@ for i in range(1,1096):
     words = f.read()
     txt.append(words)
 
-print(txt[0])
+# split the text roughly
+token = []
+for i in range(1095):
+    a = txt[i].split(' ')
+    token.append(a)
+
+# remove punctuation marks
+for i in range(len(token)):
+    token[i] = [j.strip(".,?:'!`_") for j in token[i]]
+
+# remove "\n" & "'" from abbreviation
+for i in range(len(token)):
+    for j in range(len(token[i])):
+        if "\n" in token[i][j]:
+            token[i][j] = token[i][j][1:]
+        if "'" in token[i][j]:
+            p = token[i][j].find("'")
+            token[i][j] = token[i][j][:p] + token[i][j][p+1]
+
+# Lowercasing everything
+for i in range(len(token)):
+    token[i] = [j.lower() for j in token[i]]
