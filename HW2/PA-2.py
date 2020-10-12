6d016f7d4b822347f920f37df2425a5e030865bb
poi0905 <asdfg3021524@gmail.com>
2020-10-12 16:30:20 +0800

    2020/10/12

--- HW2/PA-2.py
+++ HW2/PA-2.py
@@ -13,16 +13,26 @@ for i in range(1095):
     a = txt[i].split(' ')
     token.append(a)
 
-# remove punctuation marks
+# Lowercasing everything
 for i in range(len(token)):
-    token[i] = [j.strip(".%@(),?:'!+=`-_") for j in token[i]]
-    token[i] = [j.strip('"') for j in token[i]]
+    token[i] = [j.lower() for j in token[i]]
 
-# remove "\n" and "'" from abbreviation
+# remove "\n"
 for i in range(len(token)):
     for j in range(len(token[i])):
         if "\n" in token[i][j]:
             token[i][j] = token[i][j][1:]
+
+# remove punctuation marks & ""(produce after strip)
+for i in range(len(token)):
+    token[i] = [j.strip("><1234567890!@#$%^&*()-_=+[]\/.,?:;'{}`") for j in token[i]]
+    token[i] = [j.strip('"') for j in token[i]]
+    while "" in token[i]:
+        token[i].remove("")
+
+# "'" from abbreviation
+for i in range(len(token)):
+    for j in range(len(token[i])):
         if "'" in token[i][j] and token[i][j][-1] == "s":
             p = token[i][j].find("'")
             token[i][j] = token[i][j][:p]
@@ -36,23 +46,21 @@ for i in range(len(token)):
             p = token[i][j].find("'")
             token[i][j] = token[i][j][:p]
 
-# Lowercasing everything
+# Create a stop words list and eliminate them
+stopwordlist = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "doesn", "should", "now"]
 for i in range(len(token)):
-    token[i] = [j.lower() for j in token[i]]
+    for j in stopwordlist:
+        for k in token[i]:
+            if k == j:
+                token[i].remove(j)
+
+# print(token[993]) got 'he' can't eliminate !!
 
 # Stemming using Porter’s algorithm.
 ps = PorterStemmer()
 for i in range(len(token)):
     for j in range(len(token[i])):
         token[i][j] = ps.stem(token[i][j])
-
-# Create a stop words list and eliminate them
-stopwordlist = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "doesn", "should", "now",""]
-for i in range(len(token)):
-    for j in stopwordlist:
-        for k in token[i]:
-            if k == j:
-                token[i].remove(j)
 
 # Remove duplicates to get df
 df_token = token
@@ -67,16 +75,17 @@ totaltoken = []
 for i in range(len(df_token)):
     totaltoken.append(df_token[i])
 
-# build a dict and sort it
+# Build a dict
 df_dict = {}
 for i in range(len(totaltoken)):
     for key in token[i]:
         df_dict[key] = df_dict.get(key, 0) + 1
 
-df_dict_items = df_dict.items()
-sorted_items = sorted(df_dict_items)
-df_dict = dict(sorted_items)
-
-with open("result.txt", "w") as output:
-    output.write(str(df_dict))
-
+# Output a "dict.txt" file (sorted)
+term_index = 1
+with open("dict.txt", "w") as output:
+    for key in sorted(df_dict):
+        string= str(term_index)+"\t"+str(key)+"\t"+str(df_dict[key])
+        output.write(string)
+        output.write("\n")
+        term_index += 1
